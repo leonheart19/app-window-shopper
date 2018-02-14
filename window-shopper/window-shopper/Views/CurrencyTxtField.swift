73de139e8fe167ff38abee8b5a4a71bad6e035ce
Leonheart19 <kianti@gmail.com>
2018-02-15 06:25:33 +0800

    second commit

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Kian Ti Chua on 9/2/18.
+//  Copyright © 2018 KianTiChua. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTxtField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+            customizeView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+        }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2470840669)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+}
